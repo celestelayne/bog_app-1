4c434ac42d03f8600567ffcca554d1fe1618a32c
rycwilson <rycwilson@gmail.com>
2015-06-08 12:55:24 -0700

    corrected a comment in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get 'creatures/new', to: 'creatures#new', as: 'new_creature'
   get 'creatures/:id', to: 'creatures#show', as: 'creature'
   # Since the form_for in edit.html.erb was allowed to infer action
-  # and method from our object method, we must use patch instead of put
+  # and method from our model object, we must use patch instead of put
   patch 'creatures/:id', to: 'creatures#update'
   get 'creatures/:id/edit', to: 'creatures#edit', as: 'edit_creature'
   delete 'creatures/:id', to: 'creatures#destroy', as: 'delete_creature'
